c6571b3c14dff4c441c8e0502cd152fbdaa295b1
Anjani Rohith <ac185478@ncr.com>
2023-07-22 03:16:08 +0530

    code refactor

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const path = require("path");
-const sqlite = require("sqlite3").verbose();
-const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
+const {register} = require('./src/controllers/register.controller');
+const {db} = require('./src/controllers/db.controller');
+const { login } = require("./src/controllers/login.controller");
+
 //create an Express App
 const app = express();
-
 //middleware to parse the data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // defining and serving static pages
 app.use(express.static(path.join(__dirname, "public")));
 
 //defining the port
 const port = process.env.PORT || 3000;
-
-//Connect to sqliteDB
-const db = new sqlite.Database("database.db", (err) => {
-  if (err) {
-    console.log("error connecting to db", err.message);
-  } else {
-    console.log("connection successful");
-  }
-});
-
 let users = `CREATE TABLE IF NOT EXISTS "users" (
     "id" integer,
     "fullName" varchar DEFAULT NULL,
@@ -43,7 +35,8 @@ let tableQuery = `CREATE TABLE IF NOT EXISTS "twofa" (
         "pin" varchar DEFAULT NULL, 
         PRIMARY KEY (id)
         );`;
-//creating the table
+
+//creating the tables
 db.serialize(() => {
   db.run(tableQuery, (err) => {
     console.log(err);
@@ -51,48 +44,16 @@ db.serialize(() => {
   db.run(users,err=>{
     console.log(err);
   })
-  db.close();
+
 });
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  //Get the credentials
-  db.get(
-    "SELECT * FROM users WHERE userName = ?",
-    [username],
-    async (err, usr) => {
-      if (err) {
-        return res.status(500).json({ error: "failed to fetch user data" });
-      }
-      if (!usr) {
-        return res.status(401).json({ error: "invalid credentials" });
-      }
-      //validation of credentials
-      const passMatch = await bcrypt.compare(password, usr.password);
-      if (!passMatch) {
-        return res.status(401).json({ error: "invalid credentials" });
-      }
+app.post("/login",login);
 
-      res.status(201).json({ message: "User loggedin successfully" });
-    }
-  );
-});
-
-app.post("/register", async (req, res) => {
-  let { fullname, username, email, password } = req.body;
-  const hashedPass = await bcrypt.hash(password, 10);
-  db.run(
-    `INSERT INTO users (fullName,userName,email,password) VALUES(?,?,?,?)`,
-    [fullname, username, email, hashedPass],
-    (err) => {
-      if (err) {
-        return res.status(500).json({ error: "Failed to register user" });
-      }
-      res.status(201).json({ message: "User registered successfully" });
-    }
-  );
-});
+app.post("/register",register);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+exports.db=db;
